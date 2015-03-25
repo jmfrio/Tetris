ba4eeaf89001e6f927c8b3fea1c9728739883db7
bharrin4 <bharrin4@nd.edu>
2015-03-24 23:47:44 -0400

    test rotate and movement for box, L and J

--- testNewRotateL.cpp
+++ testNewRotateL.cpp
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include "LPiece.h"
+#include "BoxPiece.h"
+#include "jPiece.h"
 #include "Piece.h"
 
 using namespace std;
@@ -9,6 +11,7 @@ using namespace std;
 int main()	{
 
 //piece constructors
+cout << "LPiece" << endl;
 LPiece LPiece_1(4);
 
 LPiece_1.Display();
@@ -50,5 +53,80 @@ LPiece_1.left();
 cout << "Left: " << endl;
 LPiece_1.Display();
 
-}
 
+cout << "jPiece" << endl;
+//piece constructors
+jPiece jPiece_1(4);
+
+jPiece_1.Display();
+//displays the Line-piece in its initial orientation
+jPiece_1.rotate();
+jPiece_1.Display();
+jPiece_1.rotate();
+jPiece_1.Display();
+jPiece_1.rotate();
+jPiece_1.Display();
+jPiece_1.rotate();
+jPiece_1.Display();
+//Line-piece has been returned to its initial orientation
+jPiece_1.right();
+cout << "Right: " << endl;
+jPiece_1.Display();
+jPiece_1.right();
+cout << "Right: " << endl;
+jPiece_1.Display();
+jPiece_1.right();
+cout << "Right: " << endl;
+jPiece_1.Display();
+jPiece_1.down();
+cout << "Down: " << endl;
+jPiece_1.Display();
+jPiece_1.down();
+cout << "Down: " << endl;
+jPiece_1.Display();
+jPiece_1.down();
+cout << "Down: " << endl;
+jPiece_1.Display();
+jPiece_1.left();
+cout << "Left: " << endl;
+jPiece_1.Display();
+jPiece_1.left();
+cout << "Left: " << endl;
+jPiece_1.Display();
+jPiece_1.left();
+cout << "Left: " << endl;
+jPiece_1.Display();
+
+
+cout << "BoxPiece" << endl << endl;
+BoxPiece BoxPiece_1(4);
+
+BoxPiece_1.Display();
+BoxPiece_1.right();
+cout << "Right: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.right();
+cout << "Right: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.right();
+cout << "Right: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.down();
+cout << "Down: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.down();
+cout << "Down: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.down();
+cout << "Down: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.left();
+cout << "Left: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.left();
+cout << "Left: " << endl;
+BoxPiece_1.Display();
+BoxPiece_1.left();
+cout << "Left: " << endl;
+BoxPiece_1.Display();
+}
