5ad8f6bfad9c7770c4b099b7cc52d66e17eab70e
lizvista <liz.vista@yahoo.com>
2015-03-25 10:36:13 -0400

    Updated Board files

--- Board.h
+++ Board.h
@@ -10,20 +10,43 @@
 #include <vector>
 #include <iostream>
 #include <algorithm>
+#include "Piece.h"
+#include "BoxPiece.h"
+#include "LinePiece.h"
+#include "lPiece.h"
+#include "jPiece.h"
+#include "sPiece.h"
+#include "zPiece.h"
+#include "tPiece.h"
+
 using namespace std;
 
 class Board {
 	public:
 		Board();                            // constructor
 		void display();                     // output current board
-		int getID(int, int);                // return ID held in spot (x,y)
+		int isSpotFull(int, int);           // return 1 if piece is in spot (x,y), 0 otherwise
 		void fillRow(int);                  // set values in a particular row
 		int isRowFull(int);                 // returns 1 if row is full, 0 if not
 		void deleteRow(int);                // deletes row (to be used when row is filled) 
+		void addPiece(int);                 // put a new piece on the board
+		int isGameOver();                   // to be checked after every piece finished falling, before next piece is made
+		void updateCoordinates(); // set the board to match current coordinates of piece
 	private:
 		int width;                          // width of board
 		int height;                         // height of board
 		vector< vector<int> > board;        // 2D vector of ints
+		vector< vector<int> > temp;         // temp board
+		Piece newPiece;                     // currently falling piece
+		int pieceType;                      // randomly generated type of next piece
+		int newPieceR1;                     // when piece coordinates are updated, the previous ones are saved here
+		int newPieceR2;                     // this way, the old coordinates can be set to 0
+		int newPieceR3;
+		int newPieceR4;
+		int newPieceC1;
+		int newPieceC2;
+		int newPieceC3;
+		int newPieceC4;
 };
 
 
@@ -46,6 +69,10 @@ Board::Board() {
 	for( int i=0; i<height; i++ ) {
 		board.push_back(oneDvec);
 	}
+	
+	srand(time(NULL));
+	pieceType = (rand()%7) + 1;
+	addPiece( pieceType );
 }
 
 void Board::display() {
@@ -57,8 +84,12 @@ void Board::display() {
 	}
 }
 
-int Board::getID( int row, int column ) {
-	return board[row][column];
+int Board::isSpotFull( int row, int column ) {
+	if( board[row][column] == 1 ) {
+		return 1;
+	} else {
+		return 0;
+	}
 }
 
 void Board::fillRow(int row) {
@@ -81,15 +112,77 @@ int Board::isRowFull( int row ) {
 }
 
 void Board::deleteRow( int row ) {
-	for( int i=row; i>0; i-- ) {           // each row from the full row up
-		for( int j=0; j<width; j++ ) {        // each position in that row
-			board[i][j] = board[i-1][j];  // set equal to row above
+	for( int i=row; i>0; i-- ) { // each row from the full row up
+		for( int j=0; j<width; j++ ) { // each position in that row
+			board[i][j] = board[i-1][j]; // set equal to row above
 		}
 	}
-
 	for( int j=0; j<width; j++ ) {
-		board[height-1][j] = 0;               // set first row to 0s
+		board[height-1][j] = 0; // set first row to 0s
 	}
+}
+
+void Board::addPiece( int pieceType ) {
+	switch( pieceType ) {
+		case 1:
+			newPiece = new BoxPiece();
+		case 2:
+			newPiece = new LinePiece();
+		case 3:
+			newPiece = new lPiece();
+		case 4:
+			newPiece = new jPiece();
+		case 5:
+			newPiece = new tPiece();
+		case 6:
+			newPiece = new zPiece();
+		case 7:
+			newPiece = new sPiece();
+	}
+	
+	board[newPiece.r1][newPiece.c1] = 1;
+	board[newPiece.r2][newPiece.c2] = 1;
+	board[newPiece.r3][newPiece.c3] = 1;
+	board[newPiece.r4][newPiece.c4] = 1;
+	newPieceR1 = newPiece.r1;
+	newPieceR2 = newPiece.r2;
+	newPieceR3 = newPiece.r3;
+	newPieceR4 = newPiece.r4;
+	newPieceC1 = newPiece.c1;
+	newPieceC2 = newPiece.c2;
+	newPieceC3 = newPiece.c3;
+	newPieceC4 = newPiece.c4;
+}
+
+int Board::isGameOver() {
+	int isOver = 0;                         // assumes that game is not over
+	for( int i=0; i<width; i++ ) {
+		if( board[0][i] == 1 ) {
+			isOver = 1;             // if any spot in top row is full, game is over
+		}
+	}
+	return( isOver );
+}
+
+void Board::updateCoordinates() {
+	board[newPieceR1][newPieceC1] = 0;
+	board[newPieceR2][newPieceC2] = 0;
+	board[newPieceR3][newPieceC3] = 0;
+	board[newPieceR4][newPieceC4] = 0;
+
+	board[newPiece.r1][newPiece.c1] = 1;
+	board[newPiece.r2][newPiece.c2] = 1;
+	board[newPiece.r3][newPiece.c3] = 1;
+	board[newPiece.r4][newPiece.c4] = 1;
+
+	newPieceR1 = newPiece.r1;
+	newPieceR2 = newPiece.r2;
+	newPieceR3 = newPiece.r3;
+	newPieceR4 = newPiece.r4;
+	newPieceC1 = newPiece.c1;
+	newPieceC2 = newPiece.c2;
+	newPieceC3 = newPiece.c3;
+	newPieceC4 = newPiece.c4;
 }
 
 
