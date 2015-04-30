c9867a1f065353dd8c5bd684beef3eb05a2cd4a9
bharrin4 <bharrin4@nd.edu>
2015-04-30 13:56:18 -0400

    sPiece: formatted and commented

--- sPiece.h
+++ sPiece.h
@@ -1,3 +1,8 @@
+//Brittany Harrington, Elizabeth Vista, Jessie Frio
+//CSE 20212
+////Final Project: Tetris
+//sPiece .h file
+
 #ifndef SPIECE_H
 #define SPIECE_H
 #include <iostream>
@@ -8,16 +13,14 @@
 using namespace std;
 
 class sPiece : public Piece	{
- 	public:
+	public:
 		sPiece(int size=3);
-    		//virtual void fillVec();
 		virtual void rotate();
 		virtual void right();
 		virtual void left();
 		virtual void down();
 
 	private:
-	    	//int color;
 		int numR;
 };
 
